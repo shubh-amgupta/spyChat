7f917ceadc3bc6f42cb68a19ef134a5e26cce43c
Shubham Gupta <shubhamg.0907@gmail.com>
2017-10-11 01:41:14 +0530

    Minor bug fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from spy_details import Spy, ChatMessage, spy, friends #import spy details from other file
-from steganography.steganography import Steganography #Encryption of messages using Images. Install module steganography
-from datetime import datetime #Date time module
-import csv #CSV module to manipulate CSV files (Comma Seperated Values)
+from spy_details import Spy, ChatMessage, spy, friends  # import spy details from other file
+from steganography.steganography import Steganography  # Encryption of messages using Images. Install module steganography
+from datetime import datetime  # Date time module
+import csv  # CSV module to manipulate CSV files (Comma Seperated Values)
 
 
 def load_friends():
@@ -12,10 +12,11 @@ def load_friends():
             spy = Spy(row[0], row[1], row[2], row[3])
             friends.append(spy)
 
-STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.'] #List of status messages
+
+STATUS_MESSAGES = ['My name is Bond, James Bond', 'Shaken, not stirred.']  # List of status messages
 
 
-#addstatus function to manipulate status of a spy
+# addstatus function to manipulate status of a spy
 def add_status(current_status_message):
     if current_status_message != None:
         print "Your current status message is " + current_status_message + "\n"
@@ -29,12 +30,12 @@ def add_status(current_status_message):
 
         if len(new_status_message) > 0:
             updated_status_message = new_status_message
-            STATUS_MESSAGES.append(updated_status_message) #add new status to existing status's list
+            STATUS_MESSAGES.append(updated_status_message)  # add new status to existing status's list
 
-    #print list of already existing status messages
+    # print list of already existing status messages
     elif default.upper() == 'Y':
         item_position = 1
-        #for loop to display every item in a list. Usiing temporary variable (message in this case)
+        # for loop to display every item in a list. Usiing temporary variable (message in this case)
         for message in STATUS_MESSAGES:
             print str(item_position) + ". " + message
             item_position = item_position + 1
@@ -44,16 +45,15 @@ def add_status(current_status_message):
         if len(STATUS_MESSAGES) >= message_selection:
             updated_status_message = STATUS_MESSAGES[message_selection - 1]
 
-    return updated_status_message #return updated status
+    return updated_status_message  # return updated status
 
 
-
-#addfriend function to add a new friend in frineds list
+# addfriend function to add a new friend in frineds list
 def add_friend():
-    #new dictonary for every new friend addded
+    # new dictonary for every new friend addded
     new_friend = Spy('', '', 0, 0.0)
 
-    #friends Info
+    # friends Info
     new_friend.name = raw_input("Please add your friend's name: ")
     new_friend.salutation = raw_input("Are they Mr. or Ms.?: ")
     new_friend.name = new_friend.salutation + " " + new_friend.name
@@ -69,20 +69,20 @@ def add_friend():
         except:
             print 'File not available'
         friends.append(new_friend)
-        print '\nNew Friend Name " %s " Age " %d " of Rating " %.2f " Added into Friend List.' % (new_friend.name, new_friend.age, new_friend.rating)
+        print '\nNew Friend Name " %s " Age " %d " of Rating " %.2f " Added into Friend List.' % (
+        new_friend.name, new_friend.age, new_friend.rating)
 
     else:
         print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
 
-    #return length of frineds list
+    # return length of frineds list
     return len(friends)
 
 
-#selectfriend to select a frined from friends list
+# selectfriend to select a frined from friends list
 def select_friend():
-
     item_number = 0
-    #to traverse friends list
+    # to traverse friends list
     for friend in friends:
         print '%d. %s' % ((item_number + 1), friend['name'])
         item_number = item_number + 1
@@ -91,16 +91,15 @@ def select_friend():
     return friend_choice - 1
 
 
-#sendmessage to send a encoded message in an image
+# sendmessage to send a encoded message in an image
 def send_message():
-
-    #select a friend to whom message is to be send
+    # select a friend to whom message is to be send
     friend_choice = select_friend()
 
-    original_image = raw_input("What is the name of the image?") #image to be encoded (encoded.png)
-    output_path = 'secret_image.jpg' #output image. Result
-    text = raw_input("What do you want to say?") #Message you want to send encoded in image
-    Steganography.encode(original_image, output_path, text) #inbuilt function of steganography encode()
+    original_image = raw_input("What is the name of the image?")  # image to be encoded (encoded.png)
+    output_path = 'secret_image.jpg'  # output image. Result
+    text = raw_input("What do you want to say?")  # Message you want to send encoded in image
+    Steganography.encode(original_image, output_path, text)  # inbuilt function of steganography encode()
 
     # split text message to check any special message involved or not
     message = text.split(' ')
@@ -110,7 +109,7 @@ def send_message():
         if any in message:
             print "Immediate action required"
 
-    #newchat dictonary which notes timestamp of send message using datetime module
+    # newchat dictonary which notes timestamp of send message using datetime module
     new_chat = ChatMessage(text, True)
 
     friends[friend_choice]['chats'].append(new_chat)
@@ -118,7 +117,6 @@ def send_message():
 
 
 def read_message():
-
     sender = select_friend()
 
     output_path = raw_input("What is the name of the file?")
@@ -130,18 +128,19 @@ def read_message():
     print "Your secret message has been saved!"
 
 
-
 print ("Welcome to spy chat, " + spy.name)
 
-question = raw_input("Continue as " + spy.salutation + " " + spy.name + "(Y/N)? : ") #spy['name'] - it is a dictonary
+question = raw_input("Continue as " + spy.salutation + " " + spy.name + "(Y/N)? : ")  # spy['name'] - it is a dictonary
 
-#startchat function to start a chat with attributes of paricular spy
+
+# startchat function to start a chat with attributes of paricular spy
 def start_chat(spy_name, spy_age, spy_rating):
-    show_menu = True #To run a loop for displaying menu
+    show_menu = True  # To run a loop for displaying menu
     current_status_message = None
 
     while show_menu:
-        menu_choice = int(raw_input("What do you want to do? \n1. Add a status update \n2. Add a friend\n3. Select a friend\n4. Send Message\n5. Read Message\n6. Close Application\n"))
+        menu_choice = int(raw_input(
+            "What do you want to do? \n1. Add a status update \n2. Add a friend\n3. Select a friend\n4. Send Message\n5. Read Message\n6. Close Application\n"))
 
         if menu_choice == 1:
             current_status_message = add_status(current_status_message)
@@ -154,17 +153,17 @@ def start_chat(spy_name, spy_age, spy_rating):
         elif menu_choice == 5:
             read_message()
         elif menu_choice == 6:
-            show_menu = False #Exit menu
+            show_menu = False  # Exit menu
 
 
-#register a new user other than default user
+# register a new user other than default user
 if question.lower() == "n":
 
     new_user = Spy('', '', 0, 0.0)
 
-    new_user.salutation = raw_input("Should I call you Mister or Miss?: ") #Salutation for new user
+    new_user.salutation = raw_input("Should I call you Mister or Miss?: ")  # Salutation for new user
 
-    new_user.name = raw_input("Your name is " + new_user.salutation) #Salutation for new user
+    new_user.name = raw_input("Your name is " + new_user.salutation)  # Salutation for new user
 
     new_user.name = new_user.salutation + " " + new_user.name
 
@@ -174,9 +173,9 @@ if question.lower() == "n":
     new_user.rating = 0.0
     new_user.is_online = False
 
-    new_user.age = int(raw_input("What is your age?")) #age for new user
+    new_user.age = int(raw_input("What is your age?"))  # age for new user
     if 50 > new_user.age > 12:
-        new_user.rating = float(raw_input("What is your rating?")) #rating for new user
+        new_user.rating = float(raw_input("What is your rating?"))  # rating for new user
         if 4.5 < new_user.rating:
             print 'Great ace!'
         elif new_user.rating > 3.5 and new_user.rating <= 4.5:
@@ -190,7 +189,8 @@ if question.lower() == "n":
         exit()
 
     new_user.is_online = True
-    print "Authentication complete. Welcome " + new_user.name + " age: " + str(new_user.age) + " and rating of: " + str(new_user.rating) + " Proud to have you onboard"
+    print "Authentication complete. Welcome " + new_user.name + " age: " + str(new_user.age) + " and rating of: " + str(
+        new_user.rating) + " Proud to have you onboard"
 
     start_chat(new_user.name, new_user.age, new_user.rating)
 
